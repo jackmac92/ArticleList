910c3a9298543c909b42bbe6a492fab626e70d63
jackmac92 <jackmac79@gmail.com>
2016-05-19 17:01:18 -0400

    Persist sort and filter status through localstorage

--- reducers/sortParameter.js
+++ reducers/sortParameter.js
@@ -1,9 +1,11 @@
 import { SET_SORT_PARAMETER } from '../actions'
+const store = require('store')
+const initialState = store.get('sortParameter') || ["NEW", false]
 
-
-const sortParameter = (state = ["NEW",false], action) => {
+const sortParameter = (state = initialState, action) => {
   switch (action.type) {
     case SET_SORT_PARAMETER:
+    	store.set('sortParameter', action.sortParameter)
       return action.sortParameter
     default:
       return state
--- reducers/visibilityFilter.js
+++ reducers/visibilityFilter.js
@@ -1,6 +1,10 @@
-const visibilityFilter = (state = [null, null], action) => {
+const store = require('store')
+const initialState = store.get('visibilityFilter') || [null, null]
+
+const visibilityFilter = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
+    	store.set('visibilityFilter', action.visibilityFilter)
       return action.visibilityFilter
     default:
       return state
