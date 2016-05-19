8ad7a1e70aff89e663f40a5b498f1d749a380c3c
jackmac92 <jackmac79@gmail.com>
2016-05-19 13:58:05 -0400

    Add ArticleIndex w/ header components

---
+++ components/ArticleIndex.js
@@ -0,0 +1,35 @@
+import React, { PropTypes } from 'react'
+import Article from './Article'
+
+const ArticleIndex = ({ articles }) => (
+	<table className="article-index group">
+		<Header />
+	  <tbody>
+		    {articles.map(article =>
+		      <Article
+		        key={article.id}
+		        {...article}
+		      />
+		    )}
+		</tbody>
+	</table>
+)
+ArticleIndex.propTypes = {
+	articles: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		title: PropTypes.string.isRequired,
+		publish_at: PropTypes.object.isRequired,
+		shares: PropTypes.number.isRequired,
+		views: PropTypes.number.isRequired,
+		image:PropTypes.string.isRequired,
+		url:PropTypes.string.isRequired,
+		words: PropTypes.number.isRequired,
+		profile: PropTypes.object.isRequired,
+		tags:PropTypes.arrayOf(PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			name: PropTypes.string.isRequired
+		}).isRequired).isRequired
+	}).isRequired).isRequired
+}
+
+export default ArticleIndex
---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => (
+    <thead>
+      <tr>
+        <th colSpan="2">
+            Unpublished Articles
+        </th>
+        <th>
+            Author
+        </th>
+        <th>
+            Words
+        </th>
+        <th>
+            Submitted
+        </th>
+      </tr>
+    </thead>
+)
+
+export default Header
